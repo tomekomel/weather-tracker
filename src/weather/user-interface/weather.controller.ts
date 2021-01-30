15cc46fd0d26f41d40991c7b2e7a932d6065a5e7
Tomasz Omeljaniuk <tomekomel@gmail.com>
2021-01-30 18:40:19 +0100

    feat: install swagger and class validator package

--- src/weather/user-interface/weather.controller.ts
+++ src/weather/user-interface/weather.controller.ts
@@ -1,4 +1,12 @@
 import { Controller } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('weather')
 @Controller('weather')
-export class WeatherController {}
+export class WeatherController {
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
+}
