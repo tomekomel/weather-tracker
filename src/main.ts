2b22e90d9925865c509783df5492f84a3544cc28
Tomasz Omeljaniuk <tomekomel@gmail.com>
2021-01-31 22:59:06 +0100

    feat: add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Weather Tracker API')
+    .setVersion('1.0')
+    .addTag('weather')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(config.get('APP_PORT'));
 }
 bootstrap();
--- src/weather/weather.module.ts
+++ src/weather/weather.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WeatherController } from './user-interface/weather.controller';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   providers: [CommandBus, QueryBus],
   controllers: [WeatherController],
 })
