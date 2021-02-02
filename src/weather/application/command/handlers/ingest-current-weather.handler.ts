da733bce65b7f090cd77caf61050593cf8b61074
Tomasz Omeljaniuk <tomekomel@gmail.com>
2021-02-02 22:49:16 +0100

    feat: remove event-store db part 2

--- src/weather/application/command/handlers/ingest-current-weather.handler.ts
+++ src/weather/application/command/handlers/ingest-current-weather.handler.ts
@@ -1,10 +1,19 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { IngestCurrentWeatherCommand } from '../ingest-current-weather.command';
+import { Weather } from '../../../domain/weather';
+import { WeatherRepository } from '../../../infrastructure/repositories/weather.repository';
 
 @CommandHandler(IngestCurrentWeatherCommand)
 export class IngestCurrentWeatherHandler
   implements ICommandHandler<IngestCurrentWeatherCommand> {
+  constructor(private readonly weatherRepository: WeatherRepository) {}
+
   async execute(command: IngestCurrentWeatherCommand) {
-    console.log(command);
+    const { cityId, cityName } = command.getCurrentWeatherDto();
+    const weather = new Weather(cityId, cityName);
+    weather.ingest(command.getCurrentWeatherDto());
+
+    console.log('Command executed: ', command);
+    await this.weatherRepository.saveWeather(weather);
   }
 }
--- src/weather/weather.module.ts
+++ src/weather/weather.module.ts
@@ -3,14 +3,12 @@ import { WeatherController } from './user-interface/weather.controller';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ConfigModule } from '@nestjs/config';
 import { WeatherRepository } from './infrastructure/repositories/weather.repository';
-import { eventStoreProvider } from '../common/providers/event-store.provider';
 import { commandHandlers } from './application/command/handlers';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), CqrsModule],
   providers: [
     WeatherRepository,
-    eventStoreProvider,
     ...commandHandlers,
   ],
   controllers: [WeatherController],
